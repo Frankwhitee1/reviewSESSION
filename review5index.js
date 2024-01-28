57f17233571811dd9d12530e11dea166154e76c4
franklinTaylor <ftaylor07@icloud.com>
2024-01-28 12:55:44 -0500

    new solution

--- review5index.js
+++ review5index.js
@@ -37,41 +37,43 @@ function strGreaterThan5(arrOfStr) {
     return ArrOfStr;
 }
 
-// return arrOfStr.reduce((acc, str) => {
+// const strGreaterThan5 = arrOfStr.reduce((acc, str) => {
 //     if (str.length > 5) {
 //         acc.push(str);
 //     }
 //     return acc;
 // }, []
 
+// return arrayOfstr.filter(str = str.length > 5)
+
 // 3. Write a function that takes an array of strings, and returns a new array of the lengths of strings longer than 5 characters
 
 function stringsLongerThanfivee(arrOfStr){
-    // const stringslongerthanFive = [];
+    const stringslongerthanFive = [];
 
-    // for (const str of arrOfStr) {
-    //     if (str.length > 5) {
-    //         stringslongerthanFive.push(str.length);
-    //     }
-    // }
+    for (const str of arrOfStr) {
+        if (str.length > 5) {
+            stringslongerthanFive.push(str.length);
+        }
+    }
 
-    // return stringslongerthanFive;
+    return stringslongerthanFive;
 }
 
-const stringslongerthanFive =  arrOfStr.reduce((acc, str) => {
-    if (str.length > 5) {
-        acc.push(str.length);
-    } 
+// const stringslongerthanFive =  arrOfStr.reduce((acc, str) => {
+//     if (str.length > 5) {
+//         acc.push(str.length);
+//     } 
 
-    return acc;
+//     return acc;
 
-}, [] )
+// }, [] )
 
 
 // 4. Write a function that takes an array of numbers and returns the sum of all even numebrs
 
 function sumOfEven(arrOfNum) {
-    sum = 0;
+    let sum = 0;
 
     for (const num of arrOfNum) {
         if (num % 2 === 0){
@@ -92,6 +94,18 @@ function findMin(arrOfNum) {
     return Math.min(...arrOfNum)
 }
 
+
+function findMin (arr) {
+    let min = infinity;
+
+    for (const num of arrofNum) {
+        if ( num < min) {
+            min = num;
+        }
+    }
+
+    return min;
+}
 // 6. Write a function that takes in a string and returns the character that appears the most times (only letters count)
 
 function GetMostletterCount(inputStr) {
